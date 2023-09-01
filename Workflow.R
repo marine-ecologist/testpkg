3b03718e449afed4f4fc110d91dfc322287260bb
marine-ecologist <george.roff@csiro.au>
2023-09-01 13:27:34 +1000

    second commit

---
+++ Workflow.R
@@ -0,0 +1,13 @@
+library(devtools)
+
+# 1. create package
+#create("/Users/rof011/testpkg")
+
+# 2. use git (not sure why)
+#use_git()
+
+# 3 create sample vignette
+#use_vignette("test-leafletmap", title = "testing leaflet map")
+
+
+#4.
